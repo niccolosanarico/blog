aaec81d06c16370688f88b1967871e72b0290f36
Niccolo <nico@niccolosanarico.com>
2013-04-14 23:47:37 +0200

    pagination on list of posts

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,7 +1,7 @@
 class BlogController < ApplicationController
 
     def index
-        @posts = Post.where(:status=>'public').order(:created_at).reverse_order
+        @posts = Post.paginate(page: params[:page], :per_page=>7).where(:status=>'public').order(:created_at).reverse_order
     end
 
     def thanks
