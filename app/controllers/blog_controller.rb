812fdac60008086cbd66abf7191f0f2e94967318
Niccolo <nico@niccolosanarico.com>
2014-05-22 14:55:53 +0200

    Bug fix on posts view order

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,11 +1,10 @@
 class BlogController < ApplicationController
 
     def index
-        @posts = Post.paginate(page: params[:page], :per_page=>7).where(:status=>'public').order(:created_at).reverse_order
+        @posts = Post.paginate(page: params[:page], :per_page=>7).where(:status=>'public').order(:published_at).reverse_order
     end
 
     def thanks
 
     end
 end
-
